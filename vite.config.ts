9a02d0fee0d86aa24c15dd865746775aa6f025d1
estebans-tech <estebanstech@gmail.com>
2026-01-13 05:07:14 +0100

    chore: setup vitest withou server/routes

--- vite.config.ts
+++ vite.config.ts
@@ -1,58 +1,30 @@
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vitest/config'
-import { playwright } from '@vitest/browser-playwright'
 import { sveltekit } from '@sveltejs/kit/vite'
+import { svelteTesting } from '@testing-library/svelte/vite'
+export default defineConfig(({ mode }) => ({
+  plugins: [tailwindcss(), sveltekit(), svelteTesting()],
 
-const isCI = process.env.CI === 'true'
+  resolve: {
+    conditions: mode === 'test' ? ['browser'] : [],
+  },
 
-export default defineConfig({
-  plugins: [tailwindcss(), sveltekit()],
   test: {
-      projects: [
-        {
-          extends: true,
-          test: {
-            name: 'unit',
-            environment: 'jsdom',
-            setupFiles: ['./vitest.setup.ts'],
-            include: [
-              'src/**/*.{test,spec}.{ts,js}',
-              'src/**/*.svelte.{test,spec}.{ts,js}',
-            ],
-            exclude: [
-              'src/routes/**',
-              'src/**/*.browser.{test,spec}.{ts,js}',
-              'src/**/*.server.{test,spec}.{ts,js}',
-              'src/lib/server/**',
-            ],
-          },
-        },
-        {
-          extends: true,
-          test: {
-            name: 'server',
-            environment: 'node',
-            include: ['src/**/*.server.{test,spec}.{ts,js}'],
-            exclude: [
-              'src/**/*.browser.{test,spec}.{ts,js}',
-              'src/**/*.svelte.{test,spec}.{ts,js}',
-            ],
-          },
-        },
-        {
-          extends: true,
-          test: {
-            name: 'browser',
-            environment: 'jsdom',
-            setupFiles: ['./vitest.setup.ts'],
-            browser: {
-              enabled: !isCI,
-              provider: playwright(),
-              instances: [{ browser: 'chromium', headless: true }],
-            },
-            include: ['src/routes/**/*.browser.{test,spec}.{ts,js}'],
-          },
-        },
-      ],
-    },
-})
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: ['./vitest.setup.ts'],
+
+    include: [
+      'src/**/*.{test,spec}.{ts,js}',
+      'src/**/*.svelte.{test,spec}.{ts,js}',
+    ],
+
+    exclude: [
+      'src/routes/**',
+      'src/lib/server/**',
+      'src/**/*.server.{test,spec}.{ts,js}',
+      '.svelte-kit/**',
+      'node_modules/**',
+    ],
+  },
+}))
