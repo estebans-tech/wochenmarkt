41c9eebbb585f355965bf6a8495ca05908b7e1b6
estebans-tech <estebanstech@gmail.com>
2026-01-13 06:44:26 +0100

    chore: modify to support server tests

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vitest/config'
 import { sveltekit } from '@sveltejs/kit/vite'
 import { svelteTesting } from '@testing-library/svelte/vite'
+
 export default defineConfig(({ mode }) => ({
   plugins: [tailwindcss(), sveltekit(), svelteTesting()],
 
@@ -10,21 +11,37 @@ export default defineConfig(({ mode }) => ({
   },
 
   test: {
-    environment: 'jsdom',
-    globals: true,
-    setupFiles: ['./vitest.setup.ts'],
-
-    include: [
-      'src/**/*.{test,spec}.{ts,js}',
-      'src/**/*.svelte.{test,spec}.{ts,js}',
-    ],
-
-    exclude: [
-      'src/routes/**',
-      'src/lib/server/**',
-      'src/**/*.server.{test,spec}.{ts,js}',
-      '.svelte-kit/**',
-      'node_modules/**',
-    ],
-  },
+      projects: [
+        {
+          name: 'unit',
+          extends: true,
+          test: {
+            environment: 'jsdom',
+            globals: true,
+            setupFiles: ['./vitest.setup.ts'],
+            include: [
+              'src/**/*.{test,spec}.{ts,js}',
+              'src/**/*.svelte.{test,spec}.{ts,js}',
+            ],
+            exclude: [
+              'src/routes/**',
+              'src/lib/server/**',
+              'src/**/*.server.{test,spec}.{ts,js}',
+              '.svelte-kit/**',
+              'node_modules/**',
+            ],
+          },
+        },
+        {
+          name: 'server',
+          extends: true,
+          test: {
+            environment: 'node',
+            globals: true,
+            include: ['src/**/*.server.{test,spec}.{ts,js}'],
+            exclude: ['node_modules/**', '.svelte-kit/**'],
+          },
+        },
+      ],
+    },
 }))
